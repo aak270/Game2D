2b80b7d8f16c234291539d3088e6cc7a1d98109e
Anggoro Gagah <aak270@gmail.com>
2023-08-17 22:48:15 +0800

    set up sdl

--- Source/Game2D.cpp
+++ Source/Game2D.cpp
@@ -1,20 +1,50 @@
-// Game2D.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+#include <SDL.h>
+#include <stdio.h>
 
-#include <iostream>
+//Screen dimension constants
+const int SCREEN_WIDTH = 640;
+const int SCREEN_HEIGHT = 480;
 
-int main()
-{
-    std::cout << "Hello World!\n";
+int main(int argc, char* args[]) {
+    //The window we'll be rendering to
+    SDL_Window* window = NULL;
+
+    //The surface contained by the window
+    SDL_Surface* screenSurface = NULL;
+
+    //Initialize SDL
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+        printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
+    } else {
+        window = SDL_CreateWindow("Game2D", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+        if (window == NULL) {
+            printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
+        } else {
+            //Get window surface
+            screenSurface = SDL_GetWindowSurface(window);
+
+            //Fill the surface white
+            SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 0xFF, 0xFF, 0xFF));
+
+            //Update the surface
+            SDL_UpdateWindowSurface(window);
+
+            //Hack to get window to stay up
+            SDL_Event e; 
+            bool quit = false; 
+            while (quit == false) { 
+                while (SDL_PollEvent(&e)) { 
+                    if (e.type == SDL_QUIT) quit = true; 
+                } 
+            }
+        }
+    }
+
+    //Destroy window
+    SDL_DestroyWindow(window);
+
+    //Quit SDL subsystems
+    SDL_Quit();
+
+    return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
