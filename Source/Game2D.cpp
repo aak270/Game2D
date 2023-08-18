f312baece3cdae365197ee6fa84607cf5be37428
Anggoro Gagah <aak270@gmail.com>
2023-08-18 23:14:17 +0800

    load image

--- Source/Game2D.cpp
+++ Source/Game2D.cpp
@@ -1,50 +1,101 @@
 #include <SDL.h>
 #include <stdio.h>
 
-//Screen dimension constants
+// Screen dimension constants
 const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 480;
 
+// The window we'll be rendering to
+SDL_Window* window = NULL;
+
+// The surface contained by the window
+SDL_Surface* screenSurface = NULL;
+
+// The image we will load and show on the screen
+SDL_Surface* helloWorld = NULL;
+
+// Starts up SDL and creates window
+bool init();
+
+// Loads media
+bool loadMedia();
+
+// Frees media and shuts down SDL
+void close();
+
 int main(int argc, char* args[]) {
-    //The window we'll be rendering to
-    SDL_Window* window = NULL;
 
-    //The surface contained by the window
-    SDL_Surface* screenSurface = NULL;
-
-    //Initialize SDL
-    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
-        printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
+    // Starts up SDL and create window
+    if (!init()) {
+        printf("Failed to initialize!\n");
     } else {
-        window = SDL_CreateWindow("Game2D", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
-        if (window == NULL) {
-            printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
+        // Load media
+        if (!loadMedia()) {
+            printf("Failed to load media!\n");
         } else {
-            //Get window surface
-            screenSurface = SDL_GetWindowSurface(window);
+            // Apply the image
+            SDL_BlitSurface(helloWorld, NULL, screenSurface, NULL);
 
-            //Fill the surface white
-            SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 0xFF, 0xFF, 0xFF));
-
-            //Update the surface
+            // Update the surface
             SDL_UpdateWindowSurface(window);
 
             //Hack to get window to stay up
-            SDL_Event e; 
-            bool quit = false; 
-            while (quit == false) { 
-                while (SDL_PollEvent(&e)) { 
-                    if (e.type == SDL_QUIT) quit = true; 
-                } 
-            }
+            SDL_Event e; bool quit = false; while (quit == false) { while (SDL_PollEvent(&e)) { if (e.type == SDL_QUIT) quit = true; } }
         }
     }
 
-    //Destroy window
-    SDL_DestroyWindow(window);
-
-    //Quit SDL subsystems
-    SDL_Quit();
+    // Free resources and close SDL
+    close();
 
     return 0;
+}
+
+bool init() {
+    // Initialization flag
+    bool success = true;
+
+    // Initialize SDL
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+        printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
+        success = false;
+    } else {
+        // Create window
+        window = SDL_CreateWindow("Game2D", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+        if (window == NULL) {
+            printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
+            success = false;
+        } else {
+            // Get window surface
+            screenSurface = SDL_GetWindowSurface(window);
+        }
+    }
+
+    return success;
+}
+
+bool loadMedia() {
+    // Loading success flag
+    bool success = true;
+
+    // Load splash image
+    helloWorld = SDL_LoadBMP("../Assets/hello_world.bmp");
+    if (helloWorld == NULL) {
+        printf("Unable to load image %s! SDL Error: %s\n", "../../Assets/hello_world.bmp", SDL_GetError());
+        success = false;
+    }
+
+    return success;
+}
+
+void close() {
+    // Deallocate surface
+    SDL_FreeSurface(helloWorld);
+    helloWorld = NULL;
+
+    // Destroy window
+    SDL_DestroyWindow(window);
+    window = NULL;
+
+    // Quit SDL subsystems
+    SDL_Quit();
 }
